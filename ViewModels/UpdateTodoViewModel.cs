a35b6f71553fd351c890ad79a8c315d2a5a406c2
jehymeson.stefanini <jehymeson.stefanini@superdigital.com.br>
2023-12-27 15:29:48 -0300

    Add project files.

---
+++ ViewModels/UpdateTodoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MeuTodo.ViewModels
+{
+    public class UpdateTodoViewModel
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public bool Done { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
